6dfc697bf83c21eb1abfa9b185f54be9a7bda449
Christopher Lam <christopheryielam@gmail.com>
2021-11-09 18:42:46 -0500

    Refactor index.js to React Component files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './Game';
+
+function App() {
+    return (
+        <div>
+            <h1 align="center">Tic Tac Toe</h1>
+            <h3 align="center">By: Christopher Lam</h3>
+            <Game />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Board.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Square from './Square';
+
+class Board extends React.Component {
+    renderSquare(i) {
+      return (
+        <Square 
+            value={this.props.squares[i]} 
+            onClick={() => {this.props.onClick(i)}}
+        />);
+    }
+
+    renderSquareRow(counter) {
+        let result;
+        for(let i = 0; i < 3; i++) {
+            result += this.renderSquare(counter);
+            counter++;
+        }
+        return this.renderSquare(counter);
+    }
+  
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Board;
